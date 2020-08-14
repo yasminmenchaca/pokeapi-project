1a9454786185020aadd7218df159191a6df61559
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-13 19:05:42 -0500

    fetching pokeapi data and working on displaying information

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-const fetchPokeData = () => {
-    for (let i = 1; i <= 100; i++) {
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
 
+const fetchPokeData = () => {
+    for (let i = 1; i <= 150; i++) {
         fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
             .then(res => res.json())
-
             .then(data => {
+                console.log(data);
                 const dataTypes = data['types'];
+                const dataFirstType = dataTypes[0]['type']['name'];
+                const dataSecondType = dataTypes[1]['type']['name'];
+                const pokeName = capitalize(data['name']);
+                const pokeId = data['id'].toString().padStart(3, '0');
+                const pokeImg = data['sprites']['front_default']
+                const pokeWeight = data['weight'];
+                const pokeHeight =  data['height'];
 
-                const pokemon = {
-                    id: data['id'],
-                    name: data['name'],
-                    weight: data['weight'],
-                    height: data['height'],
-                    frontImage: data['sprites']['front_default'],
-                    dataFirstType: dataTypes[0],
-                    dataSecondType: dataTypes[1]
-                };
-                console.log(pokemon);
-            });
+                const pokemonList = $('.poke-cards');
+                const cardItems = $(
+                    `
+<div class="card" style="width: 18rem;">
+<img class="card-img-top" src="${pokeImg}">
+<div class="card-body">
+<p class="card-text">ID: #${pokeId}</p>
+<p class="card-text">Name: ${pokeName}</p>
+<p class="card-text">Weight: ${pokeWeight} Height: ${pokeHeight}</p>
+<p class="card-text">Type(s): ${dataFirstType}, ${dataSecondType}</p>
+</div>
+</div>
+`
+                );
+                pokemonList.append(cardItems);
+            })
+            .catch(err => console.log(err));
     }
 };
+
 fetchPokeData();
