714befd60bb5b9b9d88bba05287338bf41003ec3
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-14 13:25:33 -0500

    cleaned up code, still working on modal

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+$(document).ready(function () {
 
-const fetchPokeData = () => {
+    const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+
     for (let i = 1; i <= 150; i++) {
         fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
             .then(res => res.json())
@@ -13,38 +14,39 @@ const fetchPokeData = () => {
                 const pokeHeight = data['height'];
                 const pokeType = data.types.map((type) => type.type.name).join(', ');
 
-                //////////////////////////////// CARDS ////////////////////////////////
-                const pokemonList = $('.poke-cards');
-                const cardItems = $(`
-                <a href="#" data-toggle="modal" data-target="#exampleModalCenter">
-                <div class="card text-center">
-                <div class="card-header">
-                #${pokeId} - ${pokeName}
-                </div>
-                <img class="card-img-top" src="${pokeImg}">
-                <div class="card-body">
-                <p class="card-text">Type(s): ${pokeType}</p>
-                <p class="card-text">Height: ${pokeHeight}<br>Weight: ${pokeWeight}</p>
-                </div>
-                </div>
-                </a>`);
-                pokemonList.append(cardItems);
+//////////////////////////////// CARDS ////////////////////////////////
+                $('.poke-cards').append(`
+                 <a href="#" data-toggle="modal" data-target="#exampleModalCenter">
+                 <div class="card text-center">
+                 <div class="card-header">
+                 #${pokeId} - ${pokeName}
+                 </div>
+                 <img class="card-img-top" src="${pokeImg}">
+                 <div class="card-body">
+                 <p class="card-text">Type(s): ${pokeType}</p>
+                 <p class="card-text">Height: ${pokeHeight}<br>Weight: ${pokeWeight}</p>
+                 </div>
+                 </div>
+                 </a>`);
 
-//////////////////////////////// MODALS ////////////////////////////////
-                const pokeModal = $('.modal-popup');
-                const modalItems = $(`
+            })
+            .catch(err => console.log(err));
+    }
+
+    //////////////////////////////// MODALS ////////////////////////////////
+    $('.modal-popup').append(`
                 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
                 aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                 <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Pokemon Name</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
                 </div>
                 <div class="modal-body">
-                
+
                 </div>
                 <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -52,10 +54,5 @@ const fetchPokeData = () => {
                 </div>
                 </div>
                 </div>`)
-                pokeModal.append(modalItems);
-            })
-            .catch(err => console.log(err));
-    }
-};
 
-fetchPokeData();
+});
