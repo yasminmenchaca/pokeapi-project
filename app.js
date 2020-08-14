a854d05d5d5f73af025f7f93bc123efd76800608
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-14 09:30:42 -0500

    types are appearing on card

--- app.js
+++ app.js
@@ -5,29 +5,28 @@ const fetchPokeData = () => {
         fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
             .then(res => res.json())
             .then(data => {
+                console.log(data);
                 const pokeName = capitalize(data['name']);
                 const pokeId = data['id'].toString().padStart(3, '0');
                 const pokeImg = data['sprites']['front_default']
                 const pokeWeight = data['weight'];
                 const pokeHeight = data['height'];
-
-                const dataTypeOne = data['types'][0]['type']['name'];
-                console.log(dataTypeOne);
+                const pokeType = data.types.map((type) => type.type.name).join(', ');
 
                 const pokemonList = $('.poke-cards');
+
                 const cardItems = $(
-                    `
-<div class="card text-center">
-<div class="card-header">
-#${pokeId} - ${pokeName}
-</div>
-<img class="card-img-top" src="${pokeImg}">
-<div class="card-body">
-<p class="card-text">Type(s): </p>
-<p class="card-text">Weight: ${pokeWeight}/Height: ${pokeHeight}</p>
-</div>
-</div>
-`
+                `<div class="card text-center">
+                <div class="card-header">
+                #${pokeId} - ${pokeName}
+                </div>
+                <img class="card-img-top" src="${pokeImg}">
+                <div class="card-body">
+                <p class="card-text">Type(s): ${pokeType}</p>
+                <p class="card-text">Weight: ${pokeWeight}/Height: ${pokeHeight}</p>
+                </div>
+                </div>`
+
                 );
                 pokemonList.append(cardItems);
             })
