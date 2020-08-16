c76adc31aa3fdd50d9dee9ba26581c2ea432c407
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-16 17:16:54 -0500

    navbar in mobile view can close when clicking anywhere

--- app.js
+++ app.js
@@ -89,6 +89,17 @@ $(document).ready(function(){
         });
     });
     $('.reset').trigger("reset");
+
+    // Close Navbar when clicked outside
+    $(window).on('click', function(event){
+        // element over which click was made
+        var clickOver = $(event.target)
+        if ($('.navbar .navbar-toggler').attr('aria-expanded') == 'true' && clickOver.closest('.navbar').length === 0) {
+            // Click on navbar toggler button
+            $('button[aria-expanded="true"]').click();
+        }
+    });
+
 });
 
 fetchPokemonInfo().catch(error => console.log(error));
